8268a4ad79ffb161cd060bbf13c36f6d84cd50df
code-env <org.codeen@gmail.com>
2023-08-31 19:42:31 +0100

    initialized the theme switcher in the site

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import { ModeToggle } from "@/lib/themeToggle";
+
 export default function Home() {
-  return <section>this is were the magic happens</section>;
+  return (
+    <section>
+      this is were the magic happens
+      <ModeToggle />
+    </section>
+  );
 }
