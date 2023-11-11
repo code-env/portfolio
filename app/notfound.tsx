a9a100dc1a5c757abcfe5a971b24bb71bf30f749
code-env <org.codeen@gmail.com>
2023-11-11 09:45:04 +0100

    changes

--- app/notfound.tsx
+++ app/notfound.tsx
@@ -4,7 +4,7 @@ const NotFound = () => {
   return (
     <section className="flex text-background">
       <h1 className="text-5xl text-center">
-        OPPS! SORRY THIS PAGE CAN'T BE FOUND
+        OPPS! SORRY THIS PAGE CAN&apos;T BE FOUND
       </h1>
     </section>
   );
