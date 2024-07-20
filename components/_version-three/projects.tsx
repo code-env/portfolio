9b8b9cc1c819a0e9f1c4575e2e0bf7f25ff89992
code-env <org.codeen@gmail.com>
2024-07-20 01:02:59 +0100

    feat: removed the text of projects

--- components/_version-three/projects.tsx
+++ components/_version-three/projects.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Projects = () => {
-  return <div className="min-h-screen">Projects</div>;
+  return <div className="min-h-screen"></div>;
 };
 
 export default Projects;
