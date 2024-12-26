5aa6c90063de98adccfee9066dab9fb9c436145a
code-env <org.codeen@gmail.com>
2024-12-26 07:13:57 +0100

    feat: fixing the og image gen

--- app/(versionthree)/writings/[write]/opengraph-image.tsx
+++ app/(versionthree)/writings/[write]/opengraph-image.tsx
@@ -1,32 +1,33 @@
-/* eslint-disable jsx-a11y/alt-text */
-/* eslint-disable @next/next/no-img-element */
-import { allWritings } from "content-collections";
-import { ImageResponse } from "@vercel/og";
-import clsx from "clsx";
-import { createImageUrl } from "@/lib/images";
-import { type Writing } from "content-collections";
-
-type Params = {
-  write: string;
-};
-
-type Props = {
-  params: Params;
-};
+import { allWritings } from "@/.content-collections/generated";
+import { ImageResponse } from "next/og";
 
 export const runtime = "edge";
 
-export const size = { width: 1200, height: 630 };
+type params = {
+  write: string;
+};
+
+// Image metadata
+export const alt = "About Acme";
+export const size = {
+  width: 1200,
+  height: 630,
+};
+
 export const contentType = "image/png";
 
-export default async function Image({ params }: Props) {
-  const write = params.write;
-  // @ts-ignore i don't know how to fix typing here
-  const writing: Writing = allWritings.find((p) => write === p._meta.path);
+// Image generation
+export default async function Image({ params }: { params: params }) {
+  const slug = params.write;
+
+  const writing = allWritings.find(
+    (write) => write._meta.path === slug && write.isPublished
+  );
+
   if (!writing) {
     return new Response(
       JSON.stringify({
-        message: "Could not find writing with write: " + write,
+        message: "Could not find writing with slug: " + slug,
       }),
       {
         status: 404,
@@ -34,106 +35,37 @@ export default async function Image({ params }: Props) {
     );
   }
 
-  const SatoshiLight = fetch(
-    new URL("../../../../fonts/Satoshi-Light.otf", import.meta.url)
-  ).then((res) => res.arrayBuffer());
-
-  const Satoshibold = fetch(
-    new URL("../../../../fonts/Satoshi-Bold.otf", import.meta.url)
-  ).then((res) => res.arrayBuffer());
-
-  const SatoshiMedium = fetch(
-    new URL("../../../../fonts/Satoshi-Bold.otf", import.meta.url)
+  // Font
+  const interSemiBold = fetch(
+    new URL("@/fonts/Satoshi-Black.otf", import.meta.url)
   ).then((res) => res.arrayBuffer());
 
   return new ImageResponse(
     (
+      // ImageResponse JSX element
       <div
-        tw="w-full h-full p-4 flex"
         style={{
-          backgroundImage: "linear-gradient(to right, #0891B2, #164E63)",
-          fontFamily: '"Cabin"',
+          fontSize: 128,
+          background: "white",
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
         }}
       >
-        <div tw="rounded-xl border-2 border-stone-700 w-full h-full p-4 flex bg-stone-800 shadow-lg">
-          <img
-            width="375"
-            height="562"
-            src={createImageUrl(writing.image?.url ?? "", 375, 562)}
-            tw="rounded-xl border-2 border-stone-700"
-            style={{ objectFit: "cover" }}
-          />
-          <div tw="flex flex-col px-6 w-[740px] h-full justify-between">
-            <div tw="flex flex-col">
-              <span
-                tw={clsx("font-bold text-stone-50 mb-6", {
-                  "text-7xl": writing.title.length < 40,
-                  "text-6xl": writing.title.length >= 40,
-                })}
-                style={{
-                  fontFamily: '"Raleway"',
-                }}
-              >
-                {writing.title}
-              </span>
-              <span tw="text-5xl text-stone-300">{writing.description}</span>
-            </div>
-            <div tw="flex justify-between items-end w-full text-stone-400 text-xl">
-              <span>{writing.readingTime}</span>
-              <div
-                tw="flex text-4xl font-bold"
-                style={{
-                  fontFamily: '"Raleway"',
-                }}
-              >
-                <span tw="text-stone-50">bossadizenith</span>
-                <span tw="text-orange-400">.me</span>
-              </div>
-              <span>
-                {writing.date.substring(0, writing.date.indexOf("T"))}
-              </span>
-            </div>
-          </div>
-          <div
-            tw="w-32 h-32 border-t-2 border-t-stone-700 border-l-2 border-l-stone-700 bg-stone-800 rounded-full absolute left-[324px] bottom-[59px]"
-            style={{
-              transform: "rotate(-45deg)",
-            }}
-          />
-          <div tw="absolute left-[338px] bottom-[68px] flex items-center">
-            <img
-              tw="rounded-full border-2 border-stone-700"
-              width="110"
-              height="110"
-              src="https://avatars.githubusercontent.com/u/135658967"
-            />
-            <p tw="ml-6 text-4xl font-semibold text-stone-400">
-              Bossadi Zenith
-            </p>
-          </div>
-        </div>
+        About Acme
       </div>
     ),
+    // ImageResponse options
     {
+      // For convenience, we can re-use the exported opengraph-image
+      // size config to also set the ImageResponse's width and height.
       ...size,
-      debug: false,
       fonts: [
         {
-          name: "Raleway",
-          data: await SatoshiLight,
-          style: "normal",
-          weight: 700,
-        },
-
-        {
-          name: "Cabin",
-          data: await Satoshibold,
-          style: "normal",
-          weight: 600,
-        },
-        {
-          name: "Cabin",
-          data: await SatoshiMedium,
+          name: "Inter",
+          data: await interSemiBold,
           style: "normal",
           weight: 400,
         },
